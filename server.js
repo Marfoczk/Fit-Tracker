6584a7db6314dd84caf48c7034fb8f2a1538f54e
Marcin Głażewski <marfoczok@gmail.com>
2020-09-21 15:13:50 +0200

    upload to heroku

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 
 require('dotenv').config();
 
+// ** MIDDLEWARE / WHITELIST ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://fit-tracker-mrf.herokuapp.com/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -19,8 +35,16 @@ connection.once('open', () => {
     console.log('MongoDB database connection succesfull')
 })
 
-// const usersRouter = require('./routes/users')
-// app.use('/users', usersRouter);
+//** HEROKU ENVOIRNMENT VARIABLE **//
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 const exercisesRouter = require('./routes/exercises')
 app.use('/exercises', exercisesRouter);
